3f6e59517184a256c7cf4141b1bcb85f2d2de17b
Roque <roquecampos@gmail.com>
2022-07-14 07:55:13 +0200

    second commit

---
+++ CoolPrivatePod/CoolPrivatePod.h
@@ -0,0 +1,18 @@
+//
+//  CoolPrivatePod.h
+//  CoolPrivatePod
+//
+//  Created by Roque Campos Parada on 14.07.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoolPrivatePod.
+FOUNDATION_EXPORT double CoolPrivatePodVersionNumber;
+
+//! Project version string for CoolPrivatePod.
+FOUNDATION_EXPORT const unsigned char CoolPrivatePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoolPrivatePod/PublicHeader.h>
+
+
---
+++ CoolPrivatePod/CoolPrivatePod.swift
@@ -0,0 +1,15 @@
+//
+//  CoolPrivatePod.swift
+//  CoolPrivatePod
+//
+//  Created by Roque Campos Parada on 14.07.22.
+//
+
+import Foundation
+class CoolPrivatePod{
+    
+    func getText()->String{
+        return "CoolPrivatePod"
+    }
+    
+}
